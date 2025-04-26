c29bfc5d98891cfa57c1c4bbb03f95a6e0d0c0c3
Giovana Gomes <160107982+giovanagomesds@users.noreply.github.com>
2025-04-26 08:43:10 -0300

    login

--- administrador/conexao-banco/conexao.php
+++ administrador/conexao-banco/conexao.php
@@ -1,8 +1,8 @@
 <?php 
-$hostname = "10.67.22.216";
+$hostname = "localhost";
 $banco = "bd_tcc_etim_123_g2";
-$usuario = "us_etim_123_g2";
-$senha = "ec0623";
+$usuario = "root";
+$senha = "";
 
 $conn = new mysqli($hostname, $usuario, $senha, $banco);
 
