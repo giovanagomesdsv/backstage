d087ff6c414b86d119a432db4a1a8ae99ebbd28a
Giovana Gomes <g.gomesdsv@gmail.com>
2025-05-06 13:06:25 -0300

    esqueci a senha

---
+++ esqueci a senha/conexao-banco/conexao.php
@@ -0,0 +1,14 @@
+<?php 
+$hostname = "localhost";
+$banco = "bd_tcc_etim_123_g2";
+$usuario = "us_etim_123_g2";
+$senha = "ec0623";
+
+$conn = new mysqli($hostname, $usuario, $senha, $banco);
+
+if ($conn->connect_errno) {
+    echo "Falha ao conectar ->" . $conn->connect_errno . " ->" . $conn->connect_error;
+} else {
+  
+}
+?>
