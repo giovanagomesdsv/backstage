bc528240fbbc31bcdacdc49879023a45be022891
Giovana Gomes <160107982+giovanagomesds@users.noreply.github.com>
2025-05-07 22:05:41 -0300

    index.php
    finalização do backstage

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['email'], $_POST['senh
 
 $usuario_db agora contém os dados encontrados que foram pedidos na consulta SQL*/
 
-           if ($senha === $usuario_db['usu_senha']) {  // password_verify($senha, $usuario_db['usu_senha'])
+           if (password_verify($senha, $usuario_db['usu_senha'])) {  
                 $_SESSION['id'] = $usuario_db['usu_id'];
                 $_SESSION['nome'] = $usuario_db['usu_nome'];
                 $_SESSION['tipo'] = $usuario_db['usu_tipo_usuario'];
