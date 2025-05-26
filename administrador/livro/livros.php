cbcf335d4bb8fb1f4e5bfa6937cba5e00721a8e4
MarianaArissa123 <arissamariana66@gmail.com>
2025-05-26 14:22:06 -0300

    teste

--- administrador/livro/livros.php
+++ administrador/livro/livros.php
@@ -6,7 +6,7 @@ include "../protecao.php";
 <html lang="pt-br">
 
 <head>
-    <link rel="stylesheet" href="../livros.css">
+    <link rel="stylesheet" href="livros.css">
     <link rel="stylesheet" href="../geral.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
